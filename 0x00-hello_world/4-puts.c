ee1391546c0e5f2dd40078729414d6d37ac8b3c5
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-16 19:49:20 +0000

    Using Puts C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
-* main(void) prints "Programming is like building a multilingual puzzle"
+/**
+* main - prints "Programming is like building a multilingual puzzle"
 */
 int main(void)
 {
