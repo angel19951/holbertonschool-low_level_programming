43a28cf0df315da955cb32ad079f78e066b3d690
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-16 20:02:16 +0000

    Print with c C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+* main - Prints "with proper grammar, but the outcome is a piece of art,"
+* Return: return 0 value to function.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+
+return (0);
+}
