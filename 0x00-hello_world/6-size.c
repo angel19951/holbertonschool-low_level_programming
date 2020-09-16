b56e7f55f7c061aec5864da4ed87a47fcccd694d
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-16 21:08:27 +0000

    Size of variables C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ long int longInteger;
 long long int longlongInteger;
 float decimal;
 
-printf(sizeof(character));
-printf(sizeof(integer));
-printf(sizeof(longInteger));
-ptintf(sizeof(longlongInteger));
-printf(sizeof(decimal));
+printf("Size of a char:"sizeof(character));
+printf("Sizr of an int:"sizeof(integer));
+printf("Size of a long int:"sizeof(longInteger));
+ptintf("Size of a long long int:"sizeof(longlongInteger));
+printf("Size of a float:"sizeof(decimal));
 
 return (0);
 }
