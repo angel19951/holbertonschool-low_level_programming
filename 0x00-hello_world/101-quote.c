db07989ff35c7141ed574abada84e4aa68240ca2
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-16 22:29:30 +0000

    Dora Korpar quote C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 * main - prints "and that piece of art is useful\"" - Dora Korpar, 2015-10-19
 * Return: return empty value at end
 */
-int main()
+int main(void)
 {
 puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 return (0);
