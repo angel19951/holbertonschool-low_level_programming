18a989d741ac28d7879975fa81898fd6971e5301
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-17 18:24:26 +0000

    Find last digit C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (n > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, endValue);
 }
-if (n < 6 && n != 0)
+if (n != 0 && n < 6)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, endValue);
 }
