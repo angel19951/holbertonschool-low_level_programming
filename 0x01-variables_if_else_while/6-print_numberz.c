197ad42ca38e4b29827aeaf60692020b76a6dabf
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-17 19:59:08 +0000

    Print 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
-* main - print from 0 to 9
-*
-* Return: return 0 value at end of function
-*/
+
 int main(void)
 {
 int n;
-for (n = 0 ; n >= 9 ; n++)
+for (n = 0 ; n <= 9; n++)
 {
-putchar((n % 10) + '0');
+putchar((n % 10) +'0');
 }
 putchar('\n');
 
