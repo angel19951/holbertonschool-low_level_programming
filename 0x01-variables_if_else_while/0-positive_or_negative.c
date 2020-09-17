3e9df950b81e2dc6dbc3c5b2d9055324dc86b2a0
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-17 17:38:22 +0000

    Possitive or negative value C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stlib.h>
 #include <time.h>
 /**
 * main - Prints if a value is positive, negative or equal to 0
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d is positive\n", n);
+printf("%d is positive", n);
 }
 if (n == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero", n);
 }
 if (n < 0)
 {
-printf("%d is negative\n", n);
+printf("%d is negative", n);
 }
 
 return (0);
