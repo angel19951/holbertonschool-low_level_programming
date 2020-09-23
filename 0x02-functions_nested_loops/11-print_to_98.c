2a49ebbb58535d71bc690f332c7851ca3223ec07
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-23 20:47:19 +0000

    Print to 98 function C

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*
+*
+*
+*/
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			n = n  - 1;
+		}
+		if (n < 98)
+		{
+			n++;
+		}
+		printf ("%d, ", n);
+		
+	}
+	printf("%d", n);
+	printf("\n");
+}
