12f46b559ed4fed29555659256c57e4b909ebaee
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-25 13:00:09 +0000

    More nested loops function prototypes

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,35 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar function prototye */
+int _putchar(char);
+
+/* 0._isupper function prototype */
+int _isupper(int c);
+
+/* 1._isdigit function prototype */
+int _isdigit(int c);
+
+/* 2.mul function prototype */
+int mul(int a, int b);
+
+/* 3.print_numbers function prototype */
+void print_numbers(void);
+
+/* 4.print_most_numbers function prototype */
+void print_most_numbers(void);
+
+/* 5.more _numbers function prototype */
+void more_numbers(void);
+
+/* 6.print_line function prototype */
+void print_line(int n);
+
+/* 7.print_diagonal function prototype */
+void print_diagonal(int n);
+
+/* 8.print_square function prototype */
+void print_square(int size);
+
+/* 10. print_triangle function prototype */
+void print_triangle(int size);
