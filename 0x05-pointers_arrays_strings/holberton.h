258216d8991f489448efb46f1d156a2ed52958dc
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-28 16:44:45 +0000

    All function prototypes

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,36 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar function prototype */
+int _putchar(char);
+
+/* 0. reset_to_98 function prototype */
+void reset_to_98(int *n);
+
+/* 1. */
+void swap_int(int, int);
+
+/* 2. */
+int _strlen(char);
+
+/* 3. */
+void _puts(char);
+
+/* 4. */
+void print_rev(char);
+
+/* 5. */
+void rev_string(char);
+
+/* 6. */
+void puts2(char);
+
+/* 7. */
+void puts_half(char);
+
+/* 8. */
+void print_array(int, int);
+
+/* 9. *_strcpy function prototype */
+char *_strcpy(char, char);
+#endif
