856ff654eb35d1f60e6f32603b6b1b1582b79813
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-29 11:50:48 +0000

    Swap values pointer function C

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - changes value of variables
+ * @a: variable to be changed
+ * @b: variable to be changed
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
