88838ee7129b25ce353baa7dfd09e05f65572d36
angel19951 <agonzalezrosado123@hotmail.com>
2020-09-30 14:42:00 +0000

    All function prototypes C

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,28 +7,28 @@ int _putchar(char);
 /* 0. reset_to_98 function prototype */
 void reset_to_98(int *n);
 
-/* 1. */
+/* 1. swap_int  function prototype */
 void swap_int(int *a, int *b);
 
-/* 2. */
+/* 2. _strlen function prototype */
 int _strlen(char *s);
 
-/* 3. */
+/* 3. _puts function prototype */
 void _puts(char *str);
 
-/* 4. */
+/* 4. print_rev function prototype */
 void print_rev(char *s);
 
-/* 5. */
+/* 5. rev_string function prototype */
 void rev_string(char *s);
 
-/* 6. */
+/* 6. puts2 function prototype */
 void puts2(char *str);
 
-/* 7. */
+/* 7. puts_half function prototype */
 void puts_half(char *str);
 
-/* 8. */
+/* 8. print_array function prototype */
 void print_array(int *a, int);
 
 /* 9. *_strcpy function prototype */
