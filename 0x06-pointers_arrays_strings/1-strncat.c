72758202646773100a666919149fc7e535c322a9
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-01 20:01:36 +0000

    Strncat function C

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	int destCount, srcCount;
 
 	for (destCount = 0 ; *(dest + destCount) != '\0' ; destCount++)
-		;
+	{
+
+	}
 	for (srcCount = 0 ; *(src + srcCount) != '\0' ; srcCount++)
 	{
 		if (*(dest + destCount) < n)
@@ -20,6 +22,6 @@ char *_strncat(char *dest, char *src, int n)
 			*(dest + destCount + srcCount) = *(src + srcCount);
 		}
 	}
-	*(dest + destCount + srcCount) = '\0';
+	*(dest + srcCount + srcCount) = '\0';
 	return (dest);
 }
