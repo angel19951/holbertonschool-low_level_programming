7d207ebb0102dba7375381637965dd739f7d6796
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-01 13:32:36 +0000

    All function prototypes C

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,32 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar function prototype */
+int _putchar(char);
+
+/* 0. *_strcat function prototype */
+char *_strcat(char *dest, char *src);
+
+/* 1. *_strncat function prototype */
+char *_strncat(char *dest, char *src, int n);
+
+/* 2. *_strncpy function prototype */
+char *_strncpy(char *dest, char *src, int n);
+
+/* 3. _strcmp function prototype */
+int _strcmp(char *s1, char *s2);
+
+/* 4. reverse_array function prototype */
+void reverse_array(int *a, int n);
+
+/* 5. *string_toupper function prototype */
+char *string_toupper(char *);
+
+/* 6. *cap_string function prototype */
+char *cap_string(char *);
+
+/* 7.  */
+
+/* 8.  */
+
+#endif
