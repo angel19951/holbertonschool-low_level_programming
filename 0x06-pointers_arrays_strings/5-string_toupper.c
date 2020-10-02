5cd90c08314706d208966cee4ed3296822290768
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-02 00:58:30 +0000

    Change lower case to upper function C

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *string_toupper - changes lower cases to upper case
+ * 
+ * Return: return c
+ */
+char *string_toupper(char *toUpper)
+{
+	int counter;
+	int changer;
+
+	for (counter = 0 ; toUpper[counter] != '\0' ; counter++)
+		;
+	for (changer = 0 ; changer < counter ; changer++)
+	{
+		if(toUpper[changer] >= 'a' && toUpper[changer] <= 'z')
+		{
+			toUpper[changer] = toUpper[changer] - 'a' + 'A';
+		}
+	}
+	return (toUpper);
+}
