041ba80fcaf1f8f043e879012c715d6a54d09583
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-02 15:45:58 +0000

    Speaking in l33t function C

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ char *leet(char *s)
 	int forbidChar[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int replacerChar[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (count = 0 ; s[count] != '\0' ; count++)
+	for (count = 0; s[count] != '\0'; count++)
 	{
-		for (comparer = 0 ; forbidChar[comparer] != '\0' ; comparer++)
+		for (comparer = 0; forbidChar[comparer] != '\0'; comparer++)
 		{
 			if (s[count] == forbidChar[comparer])
 			{
