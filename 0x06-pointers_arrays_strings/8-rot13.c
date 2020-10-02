3f54d62dddf769714d72c9ffac015973143c2684
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-02 16:24:32 +0000

    Encode to rot13 function C

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *rot13 - encodes values to rot13
+ * @s - string to be encoded
+ * Return: return s
+ */
+char *rot13(char *s)
+{
+	int count;
+	int comparer;
+	char forbidChar[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char replaceChar[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		for (comparer = 0; forbidChar[comparer] != '\0'; comparer++)
+		{
+			if (s[count] == forbidChar[comparer])
+			{
+				s[count] = replaceChar[comparer];
+				break;
+			}
+		}
+	}
+	return (s);
+}
