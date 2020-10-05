29ee0f5a5cc03ebde798d504514c0fdf3ff2fa3c
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-05 18:43:54 +0000

    Copies memory area function C

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_memset - fill a block of memory with given value
+ * @s: variable to be appointed
+ * @b: replace value for memory of s
+ * @n: constant byte
+ * Return: return memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count <= n; count++)
+	{
+		*(s + count) = b;
+	}
+	return (s);
+}
