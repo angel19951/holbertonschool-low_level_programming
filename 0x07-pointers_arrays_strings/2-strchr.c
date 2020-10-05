626efbb67a4ad93fc3d9d29a3effad2c41a90bd1
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-05 19:17:45 +0000

    Locate a charecter in a string function C

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * *_strchr - locates a character in a string
+ * @s: string that will be scanned
+ * @c: character we are looking for
+ * Return: s if character is found, NULL if nothing is found
+ */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
