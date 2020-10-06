0ff6398d5dfd08f7cc084fc0dea634f699f52cff
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-06 16:47:05 +0000

    Print chess board C

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_chessboard - prints a chessboard
+ * @a: strings to be printed in chesboard
+ * Return: no return
  */
 
 void print_chessboard(char (*a)[8])
