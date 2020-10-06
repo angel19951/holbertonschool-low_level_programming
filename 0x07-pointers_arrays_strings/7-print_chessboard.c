3c13a823871eecba45c8f893153132c1e212c3b7
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-06 16:42:54 +0000

    Print chess board C

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		_putchar('\n');
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+	}
+	_putchar('\n');
+}
