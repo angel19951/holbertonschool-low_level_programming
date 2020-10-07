6393b37c93ad30c60f877fda46e028bb7204c6c7
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-07 18:25:20 +0000

    Get factorial using recursion C

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * factorial - prints the factorial of a positive number
+ * @n: nunmber to be factorized
+ * Return: -1 if n < 0; 1 if n == 0; factorial of n if n >= 1
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial ( n - 1));
+	}
+}
