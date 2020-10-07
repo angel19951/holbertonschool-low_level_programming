ee2c48e6cb96d3c6b41259fc5699d98c04c4b843
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-07 18:32:11 +0000

    Elevate to power using recursion C

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _por_recursion - elevates the value of x to the power of y
+ * @x: variable to be elevated
+ * @y: power variable
+ * Return: return -1 if y < 0, return x elevated to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if ( y < 0)
+	{
+		return -1;
+	}
+	else
+		return x * _pow_recursion(x, y - 1);
+}
