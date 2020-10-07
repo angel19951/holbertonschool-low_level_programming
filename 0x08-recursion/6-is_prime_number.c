69da05bb2ea0e19d4eb8b38e9edcccddef92ebf0
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-07 21:32:27 +0000

    Find prime number recursion C

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - checks for a prime number
+ * @n: number to be checked if prime
+ * Return: 1 if prime, 0 if not prime
+ */
+int is_prime_number(int n)
+{
+	int count;
+	int prime;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	prime = 0;
+
+	for (count = 2; count <= n / 2; count++)
+	{
+		if((n % count) == 0)
+		{
+			prime = 1;
+			break;
+		}
+	}
+	if (prime == 0)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
