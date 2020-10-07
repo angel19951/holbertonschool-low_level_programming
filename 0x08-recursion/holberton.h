0b3f8307ad69a5913a41cca36585ce1bf4600a59
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-07 17:58:11 +0000

    All function prototypes

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,31 +1,31 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-// _putchar function prototype
-_putchar(char c);
+/* _putchar function prototype */
+int _putchar(char c);
 
-// 0. void _puts_recursion function prototype
+/* 0. void _puts_recursion function prototype */
 void _puts_recursion(char *s);
 
-// 1. _print_rev_recursion function prototype
+/* 1. _print_rev_recursion function prototype */
 void _print_rev_recursion(char *s);
 
-// 2. _strlen_recursion function prototype
+/* 2. _strlen_recursion function prototype */
 int _strlen_recursion(char *s);
 
-// 3. factorial function prototype
+/* 3. factorial function prototype */
 int factorial(int n);
 
-// 4. _pow_recursion function prototype
+/* 4. _pow_recursion function prototype */
 int _pow_recursion(int x, int y);
 
-// 5. _sqrt_recursion function prototype
+/* 5. _sqrt_recursion function prototype */
 int _sqrt_recursion(int n);
 
-// 6. is_prime_number function prototype
+/* 6. is_prime_number function prototype */
 int is_prime_number(int n);
 
-// 7. is_palindrome function prototype
+/* 7. is_palindrome function prototype */
 int is_palindrome(char *s);
 
 #endif
