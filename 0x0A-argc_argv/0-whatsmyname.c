bcde2cb3167c6f8e3558dfd341990a721c400a27
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-12 00:54:54 +0000

    Print program name C

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	int index;
+	
+	for (index = 0; index < argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+	return (0);
+}
+
