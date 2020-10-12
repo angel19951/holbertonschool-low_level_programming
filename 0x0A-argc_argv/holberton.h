87821b0f7125093caca6d9cd57edc12e0bd883e3
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-12 01:00:19 +0000

    All function prototypes C

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_h
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* 0.  */
+
+/* 1.  */
+
+/* 2.  */
+
+/* 3.  */
+
+/* 4.  */
+
+/* 5.  */
+
+
+
+
+
+
+#endif
