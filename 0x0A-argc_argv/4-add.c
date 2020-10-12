5b02470cff3fc8e4f4f503de67688a867ec4f181
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-12 03:16:32 +0000

    Add positive numbers C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 /**
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 		if (*argv[index] >= 'a' && *argv[index] <= 'z')
 		{
 			printf("Error\n");
-				return (-1);
+			return (1);
 		}
 	sum += atoi(argv[index]);
 	}
