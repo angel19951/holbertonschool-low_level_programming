af1b9182655206d811e1eca3b995157bb2747c94
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-12 03:02:38 +0000

    Add positive numbers C

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add positive numbers
+ * @argc: ammount of arguments
+ * @argv: arguments
+ * Return: always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int index, sum;
+
+	if (argc >= 1)
+	{
+		for (index = 1; index < argc; index++)
+		{
+			if (*argv[index] >= 'a' && *argv[index] <= 'z')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[index]);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+		printf("Error\n");
+		return (1);
+}
