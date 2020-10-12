76814684d63cb8760d944e1de40ed76e680c3d02
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-12 00:59:25 +0000

    Print program name C

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
+ * main - prints program name without compiling
+ * @argc: size of string
+ * @argv: string
+ * Return: always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int index;
-	
+
 	for (index = 0; index < argc; index++)
 	{
 		printf("%s\n", argv[index]);
