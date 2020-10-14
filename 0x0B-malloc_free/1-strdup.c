4a5158d3fe76f02dd42bfc0b3d186889aee49a71
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-14 21:00:36 +0000

    strdup function C

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returm newly allocated space in memory
+ * @str: contains a copy of the string
+ * Return: NULL if str is null or no sufficient memory
+ */
+
+char *_strdup(char *str)
+{
+	char *dupStr;
+	char *holderStr;
+	int size;
+	int index;
+
+	for (size = 0; *(str + size) != '\0'; size++)
+		;
+
+	if (size <= 0)
+	{
+		return ('\0');
+	}
+
+	dupStr = (char *)malloc((size + 1) * sizeof(char));
+
+	if (dupStr == '\0')
+	{
+		return ('\0');
+	}
+	holderStr = dupStr;
+	for (index = 0; index < size; index++)
+	{
+		*holderStr = *str;
+		holderStr++;
+		str++;
+	}
+	*holderStr = '\0';
+	return (dupStr);
+}
