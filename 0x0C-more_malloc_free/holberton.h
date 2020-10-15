9f6609a71daa3864133350a4e1bdd62eda02cf20
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-15 16:43:28 +0000

    Header file for 0x0C. C - More malloc, free C

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* 0. Trust no one / function prototype(s) */
+void *malloc_checked(unsigned int b);
+
+/* 1. string_nconcat / function prototype(s) */
+void *malloc_checked(unsigned int b);
+
+/* 2. _calloc / function prototype(s) */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* 3. array_range / function prototype(s) */
+int *array_range(int min, int max);
+
+#endif
