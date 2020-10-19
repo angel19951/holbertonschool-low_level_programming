70df6c260463f4e9500d27955a735f58b4961106
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-19 17:52:35 +0000

    Print variables in struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints information about the dog
+ * @d: structure where data us pulled
+ * Return: if d == NULL return nothing, if a parameter is NULL print NIL
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == '\0')
+	{
+		return;
+	}
+	else
+		if (d->name == '\0')
+		{
+			printf("Name: (nil)");
+		}
+		else if (d->age == '\0')
+		{
+			printf("Age: (nil)");
+		}
+		else if (d->owner == '\0')
+		{
+			printf("Owner: (nil)");
+		}
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
+
