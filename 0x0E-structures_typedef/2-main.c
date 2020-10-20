0f991bf64bd386d982a8f25e93a9f17b285da67d
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-20 13:20:50 +0000

    Print variables in struct dog

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
@@ -8,11 +9,11 @@
  */
 int main(void)
 {
-	struct dog my_dog;
+struct dog my_dog;
 
-	my_dog.name = "Django";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
+	my_dog.name = NULL;
+	my_dog.age = .0f;
+	my_dog.owner = NULL;
 	print_dog(&my_dog);
 	return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,21 +13,19 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	else if (d->name == '\0')
+	else if (d->name != '\0')
 	{
-		printf("Name: (nil)");
-	}
-	else if (d->age == '\0')
-	{
-		printf("Age: (nil)");
-	}
-	else if (d->owner == '\0')
-	{
-		printf("Owner: (nil)");
+		printf("Name:%s\n", d->name);
 	}
 	else
-		printf("Name: %s\n", d->name);
-		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Name:(nil)\n");
+		printf("Age:%1.f\n", d->age);
+	
+	if (d->owner != '\0')
+	{
+		printf("Owner:%s\n", d->owner);
+	}
+	else
+		printf("Owner:(nil)\n");
 }
 
