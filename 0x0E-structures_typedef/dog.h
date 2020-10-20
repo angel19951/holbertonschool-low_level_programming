c127b3f5b9b452f899a3a4c08413b59979f8a4a7
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-20 16:43:20 +0000

    Dog header file with struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,22 +6,26 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+dog_t
 ;
 
-typedef struct dog dog_t;
+
 /* 1. init_dog function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /* 2. print_dog function prototype */
 void print_dog(struct dog *d);
 
-
+/* 4.create a new dog function prototype */
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlength(char *str);
+char *_strcpy(char *str);
 /*_putchar function prototype*/
 int _putchar(char c);
 #endif
