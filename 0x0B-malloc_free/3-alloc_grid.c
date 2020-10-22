c27bc66b93820ad054b0819372f941bf77807e88
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-22 19:33:50 +0000

    Allocate 2D array with malloc C

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof (int) * (width));
+		grid[i] = malloc(sizeof(int) * (width));
 		if (grid[i] == '\0')
 		{
 			i--;
