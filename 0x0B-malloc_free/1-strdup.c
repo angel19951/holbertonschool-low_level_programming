b69848b2d8f1d6b28d1cb3d995128241ecf2814c
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-22 15:38:49 +0000

    Strdup Function C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,25 +21,19 @@ char *_strdup(char *str)
 	for (size = 0; *(str + size) != '\0'; size++)
 		;
 
-	if (size == 0)
+	dupStr = (char *)malloc((size + 1) * sizeof(char));
+	if (dupStr == '\0')
 	{
 		return ('\0');
 	}
-	else
-		dupStr = (char *)malloc((size + 1) * sizeof(char));
-
-		if (dupStr == '\0')
-		{
-			return ('\0');
-		}
-		holderStr = dupStr;
-		for (index = 0; index < size; index++)
-		{
-			*holderStr = *str;
-			holderStr++;
-			str++;
-		}
-		holderStr = '\0';
+	holderStr = dupStr;
+	for (index = 0; index < size; index++)
+	{
+		*holderStr = *str;
+		holderStr++;
+	str++;
+	}
+	holderStr = '\0';
 	return (dupStr);
 	free(dupStr);
 }
