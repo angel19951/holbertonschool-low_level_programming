ba9f90c8ac33bf52a246b45009ce510594d5b025
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-22 15:45:23 +0000

    Concatenates two strings with malloc C

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	for (sizeS2 = 0; *(s2 + sizeS2) != '\0'; sizeS2++)
 		;
 	concat = sizeS1 + sizeS2 + 1;
-	str = (char *) malloc(sizeof(concat));
+	str = (char *) malloc(sizeof(concat + 1));
 	if (str == '\0')
 	{
 		return ('\0');
