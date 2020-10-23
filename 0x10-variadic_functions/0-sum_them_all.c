fb19177c104426ee2ca9d9e2f2bc24f7a63d8379
angel19951 <agonzalezrosado123@hotmail.com>
2020-10-23 18:17:37 +0000

    Sum all intigers | variadic function | C

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all paramerters
+ * @n: parameters to be summed
+ * Return: sum of parameters on succes, 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int index, sum;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start (ap, n);
+
+	sum = 0;
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg (ap, int);
+	}
+	va_end (ap);
+	return (sum);
+}
