4d0310c11e6fd6ea8eef04427c0382345ea5011b
angel19951 <2384@holbertonschool.com>
2020-10-28 22:06:04 -0400

    Adds a new node at the beginning of a list_t list | singly linked list | C

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *dup;
-	int len;
-
-	dup = strdup(str);
-	len = strlen(dup);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return(NULL);
 	}
-	new_node->len = len;
-	new_node->next = *head;
-	new_node->str = dup;
+	new_node->len = strlen(str);
+	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
 	return(0);
