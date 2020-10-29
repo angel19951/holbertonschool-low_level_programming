e047f2e9523c7b373b834be6ac13663b3f36ced3
angel19951 <2384@holbertonschool.com>
2020-10-29 14:36:28 -0400

    Add a new node at tail of a list | singly linked list | C

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,27 +1,49 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 
-
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: main node
+ * @str: value to be added
+ * Return: NULL if failed | address of the new element of succes
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *end_node, *temp_node;
+	unsigned int index = 0;
+
 	end_node = malloc(sizeof(list_t));
 	if (end_node == NULL)
 	{
-		return(NULL);
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		end_node->str = NULL;
+		end_node->len = 0;
 	}
 	else
 	{
 		end_node->str = strdup(str);
-		end_node->len = strlen(str);
-		end_node->next = NULL;
-		temp_node = *head;
-		while (temp_node != NULL)
+		while (str[index] != '\0')
+		{
+			index++;
+		}
+		end_node->len = index;
+	}
+	end_node->next = NULL;
+
+	temp_node = *head;
+	if (*head == NULL)
+	{
+		*head = end_node;
+	}
+	else
+	{
+		while (temp_node->next != NULL)
 		{
 			temp_node = temp_node->next;
 		}
-		temp_node->next = end_node;
+	temp_node->next = end_node;
 	}
-	return (*head);
+	return (end_node);
 }
