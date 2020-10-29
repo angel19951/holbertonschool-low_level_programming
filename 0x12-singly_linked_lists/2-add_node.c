46c2cfabd4b8348882bf52481f9354f7b5b38748
angel19951 <2384@holbertonschool.com>
2020-10-28 21:57:13 -0400

    Adds a new node at the beginning of a list_t list | singly linked list | C

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,8 +21,9 @@ list_t *add_node(list_t **head, const char *str)
 		return(NULL);
 	}
 	new_node->len = len;
+	new_node->next = *head;
 	new_node->str = dup;
 	new_node->next = *head;
 	*head = new_node;
-	return (0);
+	return(0);
 }
