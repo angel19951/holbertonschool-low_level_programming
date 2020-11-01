97bcb6d78b7c191ea8d0c0960b5e572970438500
angel19951 <2384@holbertonschool.com>
2020-11-01 10:32:45 -0400

    Commented prototypes until further use

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,33 +25,33 @@ int _putchar(char c);
 size_t print_listint(const listint_t *h);
 
 /* 1. List length | function prototype */
-size_t listint_len(const listint_t *h);
+/* size_t listint_len(const listint_t *h); */
 
 /* 2. Add note | function prototype */
-listint *add_nodeint(listint_t **head, const int n);
+/* listint *add_nodeint(listint_t **head, const int n); */
 
 /* 3. Add node at the end | function prototype */
-listint_t *add_nodeint_end(listint **head, const int n);
+/* listint_t *add_nodeint_end(listint **head, const int n); */
 
 /* 4.Free list | function prototype */
-void free_listint(listint_t *head);
+/* void free_listint(listint_t *head); */
 
 /* 5. Free | function prototype */
-void free_listint2(listint_t **head);
+/* void free_listint2(listint_t **head); */
 
 /* 6. Pop | function prototype */
-int pop_listint(listint_t **head);
+/* int pop_listint(listint_t **head); */
 
 /* 7. Get node at index | function prototype */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+/* listint_t *get_nodeint_at_index(listint_t *head, unsigned int index); */
 
 /* 8. Sum list | function prototype */
-int sum_listint(listint_t *head);
+/* int sum_listint(listint_t *head); */
 
 /* 9. Insert | function prototype */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+/* listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n); */
 
 /* 10. Delete at index | function prototype */
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+/* int delete_nodeint_at_index(listint_t **head, unsigned int index); */
 
 #endif
