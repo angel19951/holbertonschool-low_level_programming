9845a04f0efd82dc47cb2841ad2be0f7d334cdea
angel19951 <2384@holbertonschool.com>
2020-11-01 10:32:18 -0400

    Print all elements in a list | More singly linked list | C

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a list
+ * @h: node to be access
+ * Return: NULL if failed, count when success
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return ('\0');
+	}
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
