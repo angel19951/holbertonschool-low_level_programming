2d4ea6641796e6ce887c69039fb2b811f0c0a5f2
angel19951 <2384@holbertonschool.com>
2020-11-02 16:43:51 -0400

    Debugging for checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		unsigned int start = 1;
+		unsigned int start = 0;
 
 		hold = *head;
 		while (hold != NULL)
