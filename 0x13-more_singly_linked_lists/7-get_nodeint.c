f806426c09cb9610825cdca1d80bcd62c4dc6778
angel19951 <2384@holbertonschool.com>
2020-11-02 13:36:29 -0400

    Debbugging for checks

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,12 +14,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	temp = head;
-	if (head == NULL && temp == NULL)
+	if (head == NULL)
 	{
 		return ('\0');
 	}
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i != index; i++)
 	{
 		temp = temp->next;
 	}
