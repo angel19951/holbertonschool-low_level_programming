b3f688c17c3576bd8d27b0c3f94f939049842157
angel19951 <2384@holbertonschool.com>
2020-11-02 12:20:49 -0400

    Delete a specific nore | More singly linked lists | C

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,8 +1,15 @@
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - deletes a specific node
+ * @head: refrence to head
+ * @index: position of node to be deleted
+ * Return: 1 on success | -1 on fail
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp;
+
 	if (*head == NULL)
 	{
 		return (-1);
