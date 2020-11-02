b1bfd1c3f039bef9e77c9f2653eb21f8427cfcb9
angel19951 <2384@holbertonschool.com>
2020-11-02 13:04:37 -0400

    Gets the result of a specific node | More singly linked lists | C

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - retruns the nth of a node
+ * @head: head node
+ * @index: node to return
+ * Return: NULL if failed
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+
+	unsigned int i;
+
+	temp = head;
+	if (temp == NULL)
+	{
+		return ('\0');
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		temp = temp->next;
+	}
+	return (temp);
+}
