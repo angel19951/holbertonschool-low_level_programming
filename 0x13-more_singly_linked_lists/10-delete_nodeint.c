f91ed414543fc6061d1f099b75c8130eaeb3753c
angel19951 <2384@holbertonschool.com>
2020-11-02 12:28:38 -0400

    Delete a specific nore | More singly linked lists | C

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,18 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (count = 0; temp != NULL && count < index - 1; count++)
-	{
-		temp = temp->next;
-	}
-	if (temp == NULL || temp->next == NULL)
-	{
-		return (-1);
-	}
-
-	next = temp->next->next;
-
 	free(temp->next);
 	temp->next = next;
 }
-}
+
