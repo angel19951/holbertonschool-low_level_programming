f535124c45fe33fc96f7dfbe51c87a41c137644a
angel19951 <2384@holbertonschool.com>
2020-11-05 11:51:40 -0400

    Changes binary to unsigned int | Bitwise operators | C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,33 @@
 #include "holberton.h"
 
-unsigned int binary_to_unit(const char *b)
+/**
+ * binary_to_uint - converts binary to unsgined int
+ * @b: binary to be converted
+ * Return: 0 if failed | conversion of decimal on success
+ */
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec, idx, last;
-	unsigned int base;
+	unsigned int base = 1;
 
 	if (b == NULL)
 	{
-		return ('\0');
+		return (0);
 	}
 
+	dec = 0;
 	idx = atoi(b);
-	if (idx != 0 || idx != 1)
-	{
-		return ('\0');
-	}
-
 	while (*b)
 	{
-		last = idx / 10;
+		if (*b != '1' && *b != '0')
+		{
+			return (0);
+		}
+		last = idx % 10;
 		idx = idx / 10;
 		dec += last * base;
 		base = base * 2;
+		b++;
 	}
 	return (dec);
 }
