35d595563b0fb7d5b25ebb483022ce914c6438e4
angel19951 <2384@holbertonschool.com>
2020-11-06 15:43:52 -0400

    Prints all possible different combinations of three digits | Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int hund, dec, unit;
+
+	for (hund = '0'; hund <= '7'; hund++)
+	{
+		for (dec = '0'; dec <= '8'; dec++)
+		{
+			for (unit = '0'; unit <= '9'; unit++)
+			{
+				if (unit > dec && dec > hund)
+				{
+					putchar(hund);
+					putchar(dec);
+					putchar(unit);
+					if (hund != '7' || dec != '8' || unit != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+}
