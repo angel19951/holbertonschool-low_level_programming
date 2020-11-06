89431895b9ad7a6eeed1b7b9b1b25424358db40c
angel19951 <2384@holbertonschool.com>
2020-11-06 15:29:37 -0400

    Prints all possible combinations of two digists | Variables, if, else, while | C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int dec, units;
+
+	for (dec = '0'; dec <= '9'; dec++)
+	{
+		for (units = 0; units <= '9'; units++)
+		{
+			if (dec < units)
+			{
+			putchar(dec);
+			putchar(units);
+				if (units != '9' || dec != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
