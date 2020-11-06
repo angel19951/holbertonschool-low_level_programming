bdca2601e0d6d479d9b023e6861c461568e69628
angel19951 <2384@holbertonschool.com>
2020-11-06 14:52:13 -0400

    Sets value of a bit to 0 at a index | Bitwise manipulator | C

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,3 +6,17 @@
  * @index: bit to manipulate
  * Return: 1 on success | -1 on fail
  */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int temp = 1;
+
+	if (index > sizeof(*n) * 8)
+	{
+		return (-1);
+	}
+	temp = temp << index;
+	
+	*n &= ~temp;
+	return (1);
+}
