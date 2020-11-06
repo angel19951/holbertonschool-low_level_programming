6959048e7263475f9a5925bd0f928ca8c40d27c2
angel19951 <2384@holbertonschool.com>
2020-11-06 13:42:29 -0400

    Sets value of a bit to 1 at a given index | Bitwise operators | C

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * set_bit - Sets value of a bit to 1 at a given value
+ * @n: bit to be manipulated
+ * @index: where bit will be chaged
+ * Return: 1 on success | -1 on fail
+ */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -11,7 +17,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	}
 	temp = temp << index;
 
-	if ((temp & *n) ==0)
+	if ((temp & *n) == 0)
 	{
 	return (1);
 	}
