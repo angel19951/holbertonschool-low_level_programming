3983d1f8198bb8e2a94de6345529039dc03ebf0d
angel19951 <2384@holbertonschool.com>
2020-11-06 14:05:33 -0400

    Converts binary to int | Bitwise operators | C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,27 +7,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec, idx, last;
-	unsigned int base = 1;
+	int count;
+	unsigned int idx, sum;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	dec = 0;
-	idx = atoi(b);
-	while (*b)
+	for (count = 0; *(b + count) != '\0'; count++)
 	{
-		if (*b != '1' && *b != '0')
+		if (*(b + count) != '1' && *(b + count) != '0')
 		{
 			return (0);
 		}
-		last = idx % 10;
-		idx = idx / 10;
-		dec += last * base;
-		base = base * 2;
-		b++;
 	}
-	return (dec);
+
+	sum = 0;
+	for (count = count - 1, idx = 1; count >= 0; count--, idx = idx * 2)
+	{
+		if (*(b + count) == '1')
+		{
+			sum += idx;
+		}
+	}
+	return (sum);
 }
