cb9dc73c67c2911c42a75c2254822910b1788a9a
angel19951 <2384@holbertonschool.com>
2020-11-06 16:36:49 -0400

    Allocate memory with malloc | More malloc | C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+ * malloc_checked - allocates memory using maloc
+ * @b: memory to be allocated
+ * Return: no return for void functions
+ */
 void *malloc_checked(unsigned int b)
 {
 	int *ptr;
