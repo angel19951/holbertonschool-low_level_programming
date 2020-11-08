c1548be282fd24d79f749cbea772e8060cf5e1de
angel19951 <2384@holbertonschool.com>
2020-11-08 17:55:49 -0400

    Fixing for Betty

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,15 +7,15 @@ int _putchar(char c);
 /* 0. What's my name | Function prototype */
 void print_name(char *name, void (*f)(char *));
 
-/* 1.  If you spend too much time thinking about a thing, 
- * you'll never get it done | Function prototype */
+/* 1.  If you spend too much time thinking about a thing, */
+/* you'll never get it done | Function prototype */
 /* void array_iterator(int *array, size_t size, void (*action)(int)); */
 
-/* 2. To hell with circumstances; I create opportunities | Function prototype */
+/* 2. To hell with circumstances; I create opportunities| Function prototype */
 int int_index(int *array, int size, int (*cmp)(int));
 
-/* 3. A goal is not always meant to be reached, it often serves simply 
- * as something to aim at | Function prototype */
+/* 3. A goal is not always meant to be reached, it often serves simply */
+/* as something to aim at | Function prototype */
 int (*get_op_func(char *s))(int, int);
 
 
