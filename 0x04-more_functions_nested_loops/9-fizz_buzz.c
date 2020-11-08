dde04bfd40c2c2fa090ef120e8bd42252956cf48
angel19951 <2384@holbertonschool.com>
2020-11-08 16:47:38 -0400

    Multipliers of 3 Fizz and of 5 Buzz | More functuions and nested loops | C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 but every multiplier of 3 = Fizz, 5 = Buzz and,
+ * for 5 and 3 multipliers FizzBuzz
+ *
+ * Return: no return on void function
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", count);
+		}
+	}
+	printf("\n");
+	return (0);
+}
