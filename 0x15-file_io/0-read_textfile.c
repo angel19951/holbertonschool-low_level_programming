7576784e2f5c67e4c188ca35ead9296d153cf455
angel19951 <2384@holbertonschool.com>
2020-11-09 15:02:46 -0400

    Opens, read and writes file content in the standard output | File I/O | C

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * read_textfile - reads a text file
+ * @filename: pointer refrence to file
+ * @letters: number of letters it should read
+ * Return: 0 if can not be open, read, filename is NULL or write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n_bytes;
+	int w_out;
+	int fd = open(filename, O_RDONLY);
+	char *buffer = malloc(letters + 1);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	n_bytes = read(fd, buffer, letters);
+	if (n_bytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	*(buffer + letters) = '\0';
+	w_out = write(STDOUT_FILENO, buffer, n_bytes);
+	if (w_out == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	return(w_out);
+}
+
