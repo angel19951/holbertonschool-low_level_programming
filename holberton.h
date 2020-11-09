f1df0bc7bdfa2cb8ee54dd780cd3fdf8ebfe6a13
angel19951 <2384@holbertonschool.com>
2020-11-09 10:06:46 -0400

    All function prototypes | File I/O | C

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+/* _putchar function prototype */
+int _putchar(char c);
+
+/* 0. Tread lightly, she is near | function prototype */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1. Under the snow | function prototype */
+int create_file(const char *filename, char *text_content);
+
+/* 2. Speak gently, she can hear | function prototype */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* 3. cp | function prototype */
+
+#endif
