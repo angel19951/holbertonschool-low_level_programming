435dddf372eb2fb8fcdfca3a2e3da09c4802a340
angel19951 <2384@holbertonschool.com>
2020-11-09 21:29:36 -0400

    Fixing code for checks | C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,12 +9,12 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int w_out, count;
+	int fd;
 
 	if (filename == NULL)
 	return (-1);
 
-	int fd = open(filename, O_WRONLY | O_APPEND);
-
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		return (-1);
