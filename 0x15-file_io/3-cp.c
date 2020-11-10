17aec508507603056ac893b3f3412928bc71db3b
angel19951 <2384@holbertonschool.com>
2020-11-09 23:13:41 -0400

    Copies the content of a file to another file | File I/O | C

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,35 +2,35 @@
 
 /**
  * main - copies file contents to another file
- * @ac: argument count
- * @av: argument values
+ * @argc: argument count
+ * @argv: argument values
  * Return: always 0
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	int f_close_1, f_close_2, fd_from, fd_to, count;
 	char buff[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(av[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(av[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	fd_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	while ((count = read(fd_from, buff, 1024)) > 0)
 	{
 		if (fd_to == -1 || (write(fd_to, buff, count)) != count)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't open fd %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't open fd %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (count == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	f_close_1 = close(fd_from);
