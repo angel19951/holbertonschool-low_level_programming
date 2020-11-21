fdcae125696e263ad4d50a22fa61aa6199886425
angel19951 <2384@holbertonschool.com>
2020-11-21 13:18:47 -0400

    Fixing header file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -8,7 +8,7 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 
 /* 1. string_nconcat / function prototype(s) */
-void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 /* 2. _calloc / function prototype(s) */
 void *_calloc(unsigned int nmemb, unsigned int size);
