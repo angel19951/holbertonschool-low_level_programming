50c62d3b1c3bb0dc71d4f417647fa02e7292f21f
angel19951 <2384@holbertonschool.com>
2020-11-21 14:38:48 +0000

    Free a grid in malloc | Malloc, free | C

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a two dimensional grid
+ * @grid: grid to be freed
+ * @height: height of the grid
+ * Return: no return for void functions
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	if (height < 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+	return (grid);
+}
