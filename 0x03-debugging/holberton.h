c93ece7a0c0f130fe90560baf8ccb1f8cdb70735
angel19951 <2384@holbertonschool.com>
2020-11-23 22:34:42 -0400

    Fixing code for checkers

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,7 +5,7 @@
 void positive_or_negative(int);
 
 /* 2.largerst number function prototype */
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 
 /* 3.convert day function prototype */
 int convert_day(int, int);
