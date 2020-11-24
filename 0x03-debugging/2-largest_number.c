2ebb6a4741d7b2982ed08f91f90490c1931f0bb6
angel19951 <2384@holbertonschool.com>
2020-11-24 18:16:18 -0400

    Fixing code for checkers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && a >= c)
 	{
-		largets = a;
+		largest = a;
 	}
 	else if (b >= a && b >= c)
 	{
@@ -22,7 +22,7 @@ int largest_number(int a, int b, int c)
 	}
 	else if (c >= a && c >= b)
 	{
-		largest = c
+		largest = c;
 	}
 	return (largest);
 }
