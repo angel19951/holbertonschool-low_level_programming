bca37229f72bfebf5994568035ae02337338975d
angel19951 <2384@holbertonschool.com>
2020-12-11 11:26:54 -0400

    Print elements of a list | Doubly linked lists | C

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints a linked list
+ * @h: refrence to headof list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
