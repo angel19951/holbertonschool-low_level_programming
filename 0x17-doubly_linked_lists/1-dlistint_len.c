cc99d5228b6a03917046dce655ec56b5ae05b587
angel19951 <2384@holbertonschool.com>
2020-12-11 11:32:57 -0400

    Print length of a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints lenght of a list
+ * @h: refrence to the head of list
+ * Return: length of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	if (h == NULL)
+	{
+		return ('\0');
+	}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
