2629ae5f2040528f7db1c99fecd64b1166ad8440
angel19951 <2384@holbertonschool.com>
2020-12-13 18:38:02 -0400

    Fixing code for checks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,12 +10,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
+	if (*head != '\0')
+	{
+		(*head)->prev = new;
+	}
 	*head = new;
 	return (new);
 }
