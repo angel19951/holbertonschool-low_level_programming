1f714ca2c0075f175b9262000236360c2b0d039f
angel19951 <2384@holbertonschool.com>
2021-04-21 02:33:57 +0000

    Header file with all function prototypes

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,22 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdio.h>
+
+/*
+ * Task 0 | Function that searches for a value in a array of ints with
+ * linear search algorithm
+ */
+int linear_search(int *array, size_t size, int value);
+
+/*
+ * Task 1 | Function that searches for a value in a sorted array of ints with
+ * binary search algorithm
+ */
+int binary_search(int *array, size_t size, int value);
+
+/*
+ * Task 100 | function that searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ */
+int jump_search(int *array, size_t size, int value);
+#endif
