d86b714ed9cc498a0454a3762948174caae28285
angel19951 <2384@holbertonschool.com>
2021-04-21 16:04:53 +0000

    Uncommenting functions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,12 +10,13 @@ int linear_search(int *array, size_t size, int value);
 /*
  * Task 1 | Function that searches for a value in a sorted array of ints with
  * binary search algorithm
- *
- * int binary_search(int *array, size_t size, int value);
-*
+ */
+int binary_search(int *array, size_t size, int value);
+
+/*
  * Task 100 | function that searches for a value in a sorted array of integers
  * using the Jump search algorithm
- *
- * int jump_search(int *array, size_t size, int value);
  */
+int jump_search(int *array, size_t size, int value);
+
 #endif
